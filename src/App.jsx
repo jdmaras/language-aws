c77369eb315e0f47fdd62ada4b3b7a6aa252d1a6
Zach Kusz <zack.kusz@capitollien.com>
2023-06-05 21:36:50 -0500

    added tailwind styles with dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { useState } from 'react'
 
 function App() {
@@ -27,16 +26,22 @@ function App() {
     setTranslatedText(replaceWords.join(' '));
   }
   return (
-    <> 
-      <h1>IB Language</h1>
-      <h3>Translate</h3>
-      <input type="text" onChange={(e) => setData(e.target.value)} />
-      <button onClick={() => translateText()}>Submit</button>
-      <button onClick={() => {setPrint(false); setData('');}}>Clear</button>
-      { print ?
-      <h2>{translatedText}</h2>
-        : ''
-      }
+    <>
+      <h1 className="text-3xl text-center m-4">IB Language</h1>
+      <main className="flex flex-col items-center">
+        <h3 className="text-lg">Translate</h3>
+        <div className="flex flex-col items-center w-full">
+          <input className="rounded-md p-2 m-2 text-black w-full max-w-lg" type="text" placeholder='type to translate' onChange={(e) => setData(e.target.value)} />
+          <div>
+            <button className="rounded-md bg-zinc-950 p-4 m-2" onClick={() => {setPrint(false); setData('');}}>Clear</button>
+            <button className="rounded-md bg-zinc-950 p-4 m-2" onClick={() => translateText()}>Submit</button>
+          </div>
+        </div>
+        {print ?
+          <h2 className="p-8">{translatedText}</h2>
+            : ''
+        }
+      </main>
     </>
   )
 }
