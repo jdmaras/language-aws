a9f15de4991c00b7f17c91c29b5bd9f9272c1d60
jdmaras <john.maras@capitollien.com>
2023-06-06 16:09:00 -0500

    added a form element and put in a prevent default to keep from refreshing page

--- src/App.jsx
+++ src/App.jsx
@@ -22,26 +22,38 @@ function App() {
       }
       return word;
     })
-    console.log(replaceWords.join(' '))
     setTranslatedText(replaceWords.join(' '));
   }
+  
   return (
     <>
-      <h1 className="text-3xl text-center m-4">IB Language</h1>
-      <main className="flex flex-col items-center">
-        <h3 className="text-lg">Translate</h3>
-        <div className="flex flex-col items-center w-full">
-          <input className="rounded-md p-2 m-2 text-black w-full max-w-lg" type="text" placeholder='type to translate' onChange={(e) => setData(e.target.value)} />
-          <div>
-            <button className="rounded-md bg-zinc-950 p-4 m-2" onClick={() => {setPrint(false); setData('');}}>Clear</button>
-            <button className="rounded-md bg-zinc-950 p-4 m-2" onClick={() => translateText()}>Submit</button>
-          </div>
-        </div>
-        {print ?
-          <h2 className="p-8">{translatedText}</h2>
-            : ''
-        }
-      </main>
+      <form>
+        <h1 className="text-3xl text-center m-4">IB Language</h1>
+        <main className="flex flex-col items-center">
+          <h3 className="text-lg">Translate</h3>
+            <div className="flex flex-col items-center w-full">
+              <input className="rounded-md p-2 m-2 text-black w-full max-w-lg" 
+                type="text" 
+                placeholder='type to translate' 
+                value={data} onChange={(e) => setData(e.target.value)} />
+              <div>
+                <button className="rounded-md bg-zinc-950 p-4 m-2" 
+                  onClick={() => {setPrint(false); setData('');}}>Clear</button>
+                <input className="rounded-md bg-zinc-950 p-4 m-2" 
+                  type="submit" 
+                  onClick={(event) => {event.preventDefault(); 
+                      translateText();
+                    }
+                  } 
+                />
+              </div>
+            </div>
+              {print ?
+                <h2 className="p-8">{translatedText}</h2>
+                  : ''
+              }
+        </main>
+      </form>
     </>
   )
 }
